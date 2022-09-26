b6e74ff33311cbf4309b384abe66c897e4d6c484
Lewis Benson <tm.lewis.benson@gmail.com>
2022-09-26 15:47:39 -0400

    create random num function, create constructor function

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 'use strict';
 
 /*Globals */
+const imgObjectArray = [];
+let guesses = 25;
 
 /*Utility Functions */
+const randomNum = function () {
+  return Math.floor(Math.random() * imgObjectArray.length);
+};
 
 /*Constructors */
+function Image(name, image){
+  this.name = name;
+  this.image = image;
+  this.alt = `An image of ${name}`;
+  this.views = 0;
+  this.click = 0;
+
+  imgObjectArray.push(this);
+}
 
 /*Prototype methods */
 

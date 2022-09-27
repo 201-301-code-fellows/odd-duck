c25a389e9caa51a7c7734a2047a5630610cc0e5b
Lewis Benson <tm.lewis.benson@gmail.com>
2022-09-26 21:28:01 -0400

    add feature to update images to new images on click; add to vote count; update view count; display results

--- js/app.js
+++ js/app.js
@@ -48,7 +48,6 @@ Image.prototype.addClick = function () {
 
 Image.prototype.render = function () {
   let imgElement = document.createElement('img');
-  console.log(this.image);
   imgElement.setAttribute('src', this.image);
   imgElement.setAttribute('class', 'image');
   imgElement.setAttribute('alt', this.alt);
@@ -79,7 +78,6 @@ new Image('wine-glass', '/img/wine-glass.jpg');
 
 /* Build HTML */
 function getNewImages() {
-
   let randomImageIndex = randomNum(); //Store an array of 3 random unique numbers
   for (let i = 0; i < numberOfImages; i++) {
     imgObjectArray[imgObjectArray[randomImageIndex[i]].render()];
@@ -90,26 +88,44 @@ getNewImages(); // Get initial 3 images
 /*Event Handlers */
 let imgElement = document.querySelectorAll('.image');
 
-function voteClicking(e) {
-  for (let image of imgObjectArray) {
-    if (e.target.alt === image.alt) {
-      image.addClick();
-      guesses--;
-      getNewImages();
+function updateImgs() {
+  const newIndexArray = randomNum();
+  for (let i = 0; i < newIndexArray.length; i++) {
+    imgElement[i].setAttribute('src', imgObjectArray[newIndexArray[i]].image);
+    imgElement[i].setAttribute('alt', imgObjectArray[newIndexArray[i]].alt);
+    for (let img of imgObjectArray) {
+      if (img.alt === imgElement[0].alt) {
+        img.views++;
+      }
     }
   }
-  if (guesses === 0) {
-    //todo build button
-    // todo remove images
-    for (let images of imgElement) {
-      images.remove();
-    }
+}
+
+function outOfGuesses() {
+  for (let imgs of imgElement) {
+    imgs.remove();
   }
   h3.innerText = 'Click the button to view the results';
   section.appendChild(h3);
   buttonElement.innerText = 'Results';
   section.appendChild(buttonElement);
   section.appendChild(ul);
+}
+
+function voteClicking(e) {
+  for (let image of imgObjectArray) {
+    if (e.target.alt === image.alt) {
+      image.click++;
+      guesses--;
+      console.log(guesses);
+    }
+  }
+
+  if (guesses > 0) {
+    updateImgs();
+  } else if (guesses === 0) {
+    outOfGuesses();
+  }
 }
 
 function createResults() {
